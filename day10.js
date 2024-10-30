ea621ac0c5954bfc314d71cc714c65d9f44f13e6
talarisulochana <talarisulochana123@gmail.com>
2024-10-30 19:15:51 +0530

    Add files via upload

---
+++ day10.js
@@ -0,0 +1,20 @@
+function studentName() {
+    var name = "Sulochana"
+    var edu = "MCA";
+    var age = 22;
+    console.log("Name:",name);
+    console.log("Education:", edu);
+    console.log("Age:", age);
+}
+
+function surname() {
+    console.log("Talari");
+
+    var marks = 7.9;
+    var grade = "B+";
+    console.log("Marks:", marks);
+    console.log("Grade:", grade);
+}
+
+studentName();
+surname();
